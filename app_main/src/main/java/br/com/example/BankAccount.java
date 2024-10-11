dc95ed30b880ad0ad249063995a6c7748e70e93a
jtigik <jeftigik@hotmail.com>
2024-10-11 00:11:11 -0300

    Update pom.xml to compiler version

---
+++ app_main/src/main/java/br/com/example/BankAccount.java
@@ -0,0 +1,27 @@
+package br.com.example;
+
+public class BankAccount {
+
+    private int balance = 0;
+
+    public BankAccount(int startingBalance) {
+        this.balance = startingBalance;
+    }
+
+    public boolean withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public int deposit(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
