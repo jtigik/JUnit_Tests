37592215bd8d5023e74e46699e29f9a4106d3610
jtigik <jeftigik@hotmail.com>
2024-10-17 01:14:37 -0300

    New features to Current Class Test

--- app_main/src/main/java/br/com/example/Main.java
+++ app_main/src/main/java/br/com/example/Main.java
@@ -1,7 +1,13 @@
 package br.com.example;
 
 public class Main {
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main calc = new Main();
+        System.out.println(calc.soma(3, 4));
     }
 }
--- app_main/src/test/java/CalculaTest.java
+++ app_main/src/test/java/CalculaTest.java
@@ -1,10 +1,15 @@
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import br.com.example.Main;
 
 public class CalculaTest {
 
+    Main ct = new Main();
+
     @Test
     public void testaSoma() {
-
+        assertEquals(13, ct.soma(5, 8));
     }
 }
