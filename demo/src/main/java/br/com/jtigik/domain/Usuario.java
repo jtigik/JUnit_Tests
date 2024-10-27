5df6ad6107f942e3fa708b79e0ac325f920b0ee6
jtigik <jeftigik@hotmail.com>
2024-10-26 22:56:28 -0300

    Class Update #9

---
+++ demo/src/main/java/br/com/jtigik/domain/Usuario.java
@@ -0,0 +1,33 @@
+package br.com.jtigik.domain;
+
+public class Usuario {
+
+    private final long id;
+    private final String nome;
+    private final String email;
+    private final String senha;
+
+    public Usuario(long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
