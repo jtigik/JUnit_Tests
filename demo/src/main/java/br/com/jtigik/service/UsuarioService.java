b56009fb7bbeef98ad3c8b2adba8b8d4f45b2504
jtigik <jeftigik@hotmail.com>
2024-11-04 00:13:02 -0300

    Creating new layer called 'Services'

---
+++ demo/src/main/java/br/com/jtigik/service/UsuarioService.java
@@ -0,0 +1,18 @@
+package br.com.jtigik.service;
+
+import br.com.jtigik.domain.Usuario;
+
+public class UsuarioService {
+
+    public Usuario Salvar(Usuario usuario) {
+        /*
+         * Conectar com o Banco
+         * Preparar o SQL
+         * Executar a Consulta
+         * Obter o Usuário persistido
+         */
+
+        return usuario;
+    }
+
+}
