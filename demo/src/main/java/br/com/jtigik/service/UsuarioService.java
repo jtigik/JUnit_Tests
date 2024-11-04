e080d9cc767286be8163fe7892cd76c549b6c6d2
jtigik <jeftigik@hotmail.com>
2024-11-04 01:01:41 -0300

    Implementing the concept 'Inverter of Dependency'

--- demo/src/main/java/br/com/jtigik/service/UsuarioService.java
+++ demo/src/main/java/br/com/jtigik/service/UsuarioService.java
@@ -1,18 +1,21 @@
 package br.com.jtigik.service;
 
 import br.com.jtigik.domain.Usuario;
+import br.com.jtigik.domain.exceptions.ValidationException;
 import br.com.jtigik.service.repository.UsuarioRepository;
 
 public class UsuarioService {
 
-    private UsuarioRepository repository;
+    private final UsuarioRepository repository;
 
     public UsuarioService(UsuarioRepository repository) {
         this.repository = repository;
     }
 
     public Usuario Salvar(Usuario usuario) {
-
+        repository.getUserByEmail(usuario.getEmail()).ifPresent(user -> {
+            throw new ValidationException(String.format("Usuário %s já cadastrado!", usuario.getEmail()));
+        });
         return repository.salvar(usuario);
     }
 
