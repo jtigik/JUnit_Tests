02f3f574c622d9fdbe4301f0313eccbd3f34619f
jtigik <jeftigik@hotmail.com>
2024-11-06 01:18:59 -0300

    Implementing infra resources

--- demo/src/main/java/br/com/jtigik/infra/UsuarioMemoryRepository.java
+++ demo/src/main/java/br/com/jtigik/infra/UsuarioMemoryRepository.java
@@ -1,20 +1,44 @@
 package br.com.jtigik.infra;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import br.com.jtigik.domain.Usuario;
 import br.com.jtigik.service.repository.UsuarioRepository;
 
-public class UsuarioMemoryRepository implements UsuarioRepository {
+public final class UsuarioMemoryRepository implements UsuarioRepository {
+
+    private final List<Usuario> users;
+    private Long currentId;
+
+    public UsuarioMemoryRepository() {
+        this.currentId = 0L;
+        this.users = new ArrayList<>();
+        salvar(new Usuario(currentId, "User #1", "user1@mail.com", "123456"));
+    }
 
     @Override
     public Usuario salvar(Usuario usuario) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return new Usuario(nextId(), usuario.getNome(), usuario.getEmail(), usuario.getSenha());
     }
 
     @Override
     public Optional<Usuario> getUserByEmail(String email) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return users.stream().
+                filter(user -> user.getEmail().equalsIgnoreCase(email)).findFirst();
     }
 
+    public void printUsers() {
+        System.out.println(users);
+    }
+
+    private Long nextId() {
+        return ++currentId;
+    }
+
+    public static void main(String[] args) {
+        UsuarioMemoryRepository repo = new UsuarioMemoryRepository();
+        repo.printUsers();
+    }
 }
