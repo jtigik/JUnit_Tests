cf3e2f17901385a54b046b2a7eb011e5eecca212
jtigik <jeftigik@hotmail.com>
2024-11-09 00:07:28 -0300

    Classes Updated

--- demo/src/test/java/br/com/jtigik/infra/UserServiceComUserMemoryRepositoryTest.java
+++ demo/src/test/java/br/com/jtigik/infra/UserServiceComUserMemoryRepositoryTest.java
@@ -6,16 +6,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import br.com.jtigik.domain.Usuario;
+import br.com.jtigik.domain.exceptions.ValidationException;
 import br.com.jtigik.service.UsuarioService;
 
 public class UserServiceComUserMemoryRepositoryTest {
 
-    private UsuarioService service = new UsuarioService(new UsuarioMemoryRepository());
+    private static UsuarioService service = new UsuarioService(new UsuarioMemoryRepository());
 
     @Test
     public void devSalvarUsuarioValido() {
+
         Usuario user = service.salvar(umUsuario().comId(null).agora());
 
         Assertions.assertNotNull(user.getId());
+
+        Assertions.assertEquals(2L, user.getId());
     }
+
+    @Test
+    public void deveRejeitarUsuarioExistente() {
+        ValidationException ex = Assertions.assertThrows(ValidationException.class, ()
+                -> service.salvar(umUsuario().comId(null).agora())
+        );
+
+        Assertions.assertEquals("expected", ex.getMessage());
+    }
+
 }
