7b3ae8b3242894c769b12188de1331c57e6b3203
jtigik <jeftigik@hotmail.com>
2024-11-10 19:16:21 -0300

    Service Layer was Updated

--- demo/src/test/java/br/com/jtigik/infra/UserServiceComUserMemoryRepositoryTest.java
+++ demo/src/test/java/br/com/jtigik/infra/UserServiceComUserMemoryRepositoryTest.java
@@ -1,19 +1,23 @@
 package br.com.jtigik.infra;
 
-import static br.com.jtigik.domain.builder.UsuarioBuilder.*;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import br.com.jtigik.domain.Usuario;
+import static br.com.jtigik.domain.builder.UsuarioBuilder.umUsuario;
 import br.com.jtigik.domain.exceptions.ValidationException;
 import br.com.jtigik.service.UsuarioService;
 
+@TestMethodOrder(MethodOrderer.class)
 public class UserServiceComUserMemoryRepositoryTest {
 
-    private static UsuarioService service = new UsuarioService(new UsuarioMemoryRepository());
+    private static final UsuarioService service = new UsuarioService(new UsuarioMemoryRepository());
 
     @Test
+    @Order(1)
     public void devSalvarUsuarioValido() {
 
         Usuario user = service.salvar(umUsuario().comId(null).agora());
@@ -24,12 +28,13 @@ public class UserServiceComUserMemoryRepositoryTest {
     }
 
     @Test
+    @Order(2)
     public void deveRejeitarUsuarioExistente() {
         ValidationException ex = Assertions.assertThrows(ValidationException.class, ()
                 -> service.salvar(umUsuario().comId(null).agora())
         );
 
-        Assertions.assertEquals("expected", ex.getMessage());
+        Assertions.assertEquals("Usuário user@mail.com já cadastrado!", ex.getMessage());
     }
 
 }
