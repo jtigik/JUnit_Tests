7e8c24f271ef139752c58c796bc86d7286260d2e
jtigik <jeftigik@hotmail.com>
2024-11-29 22:57:14 -0300

    Created new enum within an interface

---
+++ demo/src/main/java/br/com/jtigik/service/event/ContaEvent.java
@@ -0,0 +1,15 @@
+package br.com.jtigik.service.event;
+
+import br.com.jtigik.domain.Conta;
+
+public interface ContaEvent {
+
+    public enum EventType {
+        CREATED,
+        UPDATED,
+        DELETED
+    }
+
+    void dispatch(Conta conta, EventType type);
+
+}
