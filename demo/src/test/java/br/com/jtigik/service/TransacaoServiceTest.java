b068d76fd8042275e155a0338e6cad7e7d16443b
jtigik <jeftigik@hotmail.com>
2024-12-10 00:04:45 -0300

    Added ParameterizedTest method

--- demo/src/test/java/br/com/jtigik/service/TransacaoServiceTest.java
+++ demo/src/test/java/br/com/jtigik/service/TransacaoServiceTest.java
@@ -1,17 +1,26 @@
 package br.com.jtigik.service;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.jtigik.domain.Conta;
 import br.com.jtigik.domain.Transacao;
+import static br.com.jtigik.domain.builder.ContaBuilder.umaConta;
 import static br.com.jtigik.domain.builder.TransacaoBuilder.umaTransacao;
+import br.com.jtigik.domain.exceptions.ValidationException;
 import br.com.jtigik.service.repositories.TransacaoDao;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,6 +55,30 @@ public class TransacaoServiceTest {
                             }
                     );
                 }
+        );
+    }
+
+    @ParameterizedTest(name = "{6}")
+    @MethodSource(value = "cenariosObrigatorios")
+    public void deveValidarCamposObrigatoriosAoSalvar(
+            Long id, String descricao, Double valor, LocalDate data, Conta conta, Boolean status, String mensagem
+    ) {
+        String exMessage = Assertions.assertThrows(ValidationException.class, () -> {
+            Transacao transacao
+                    = umaTransacao().comId(id).comDescricao(descricao).comValor(valor).comData(data).comStatus(status).comConta(conta).agora();
+            service.salvar(transacao);
+        }).getMessage();
+
+        Assertions.assertEquals(mensagem, exMessage);
+    }
+
+    @SuppressWarnings("unused")
+    static Stream<Arguments> cenariosObrigatorios() {
+        return Stream.of(
+                Arguments.of(1L, null, 10D, LocalDate.now(), umaConta().agora(), true, "Descrição inexistente"),
+                Arguments.of(1L, "Descrição", null, LocalDate.now(), umaConta().agora(), true, "Valor inexistente"),
+                Arguments.of(1L, "Descrição", 10D, null, umaConta().agora(), true, "Data inexistente"),
+                Arguments.of(1L, "Descrição", 10D, LocalDate.now(), null, true, "Conta inexistente")
         );
     }
 }
