882f0bf0912523f5a83704d3f5123aec677e27c8
jtigik <jeftigik@hotmail.com>
2024-12-09 21:47:29 -0300

    Implemented Assert multi layer

--- demo/src/test/java/br/com/jtigik/service/TransacaoServiceTest.java
+++ demo/src/test/java/br/com/jtigik/service/TransacaoServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.jtigik.service;
 
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,6 +30,22 @@ public class TransacaoServiceTest {
 
         Transacao transacaoSalva = service.salvar(transacaoParaSalvar);
 
-        Assertions.assertNotNull(transacaoSalva.getId());
+        Assertions.assertEquals(umaTransacao().agora(), transacaoSalva);
+
+        Assertions.assertAll("transação",
+                () -> assertEquals(1L, transacaoSalva.getId()),
+                () -> assertEquals("Transação Válida", transacaoSalva.getDescricao()),
+                () -> {
+                    assertAll("Conta",
+                            () -> assertEquals("Conta Válida", transacaoSalva.getConta().nome()),
+                            () -> {
+                                assertAll("Usuário",
+                                        () -> assertEquals("Usuário válido", transacaoSalva.getConta().usuario().getNome()),
+                                        () -> assertEquals("123456", transacaoSalva.getConta().usuario().getSenha())
+                                );
+                            }
+                    );
+                }
+        );
     }
 }
